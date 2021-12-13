02443f1410bc327b03c65f79444f44a58a86d4d0
kelvinbrucelee <kelvinbrucelee@hotmail.com>
2021-12-12 23:46:39 -0300

    Modificação em todo o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,32 @@ import { Card } from 'react-bootstrap';
 
 import './App.css'
 import Clima from './componets/Clima'
-import background from "./img/clima.png";
-
-var sectionStyle = {
-    position: "relative",
-    height: "100vh",
-    width: "100vw",
-    backgroundSize: "cover",
-    backgroundImage: `url(${background})`
-};
 
 export default _ =>
-    <div className="App" style={sectionStyle}>
+    <div className="App">
         <Card className="Tit">
-            <Card.Body>CLIMA DA LOCALIZAÇÃO ATUAL</Card.Body>
+            <Card.Body class="fw-light mt-3 text-white">TEMPO E TEMPERATURA</Card.Body>
         </Card>
         <div className="Cards">
             <Clima>
             </Clima>
         </div>
+        <body class="d-flex flex-column">
+            <div id="page-content">
+                <div class="container text-center">
+                    <div class="row justify-content-center">
+                        <div class="col-md-7">
+                            <h1 class="fw-light mt-4 text-white">Clima da localização atual</h1>
+                            <p class="lead text-white-50">Saiba da previsão do tempo da sua localização atual</p><br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+                <div class="container text-center">
+                    <small>Desenvolvedor &copy; Kelvin Brucelee</small>
+                </div>
+            </footer>
+        </body>
+
     </div>
--- src/componets/Clima.jsx
+++ src/componets/Clima.jsx
@@ -3,10 +3,28 @@ import "./Clima.css"
 import { OverlayTrigger, Card, ProgressBar, Badge, ListGroupItem, ListGroup, Tooltip } from 'react-bootstrap';
 import axios from 'axios'
 
-export default props => {
+// Obtém a data/hora atual
+var data = new Date();
 
+// Guarda cada pedaço em uma variável
+var dias = new Array(
+  'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado');
+var months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julio", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+var mes = data.getMonth();          // 0-11 (zero=janeiro)
+var ano4 = data.getFullYear();       // 4 dígitos
+
+// Formata a data e a hora (note o mês + 1)
+var str_data = (mes + 1) + ' ' + months[data.getMonth()] + ' ' + ano4;
+
+export default props => {
   const [location, setLocation] = useState(false);
   const [weather, setWeather] = useState(false);
+
+  function letraMaiuscula(str) {
+    var subst = str.toLowerCase().replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
+    return subst;
+  }
 
   let getWeather = async (lat, long) => {
     let res = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
@@ -15,7 +33,8 @@ export default props => {
         lon: long,
         appid: "61a6c6f7a81962b8d69cc80d900a8543",
         lang: 'pt',
-        units: 'metric'
+        units: 'metric',
+        icon: 'icon'
       }
     });
     setWeather(res.data);
@@ -34,8 +53,8 @@ export default props => {
         <Card.Img variant="top" src={process.env.PUBLIC_URL + '/img/localizacao.png'} />
         <Card.Body>
         </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroupItem>Você precisa habilitar a localização no browser</ListGroupItem>
+        <ListGroup >
+          <ListGroupItem class="fw-light mt-4 text-white">Você precisa habilitar a localização no browser</ListGroupItem>
         </ListGroup>
       </Card>
     )
@@ -48,20 +67,21 @@ export default props => {
   } else {
     return (
       <Card className="Card" border="dark" style={{ width: '23rem' }}>
-        <Card.Header className="text-center" as="h5">Clima nas suas Coordenadas:<br /> {weather['weather'][0]['description']}°</Card.Header>
-        <Card.Body>
+        <Card.Header className="header-color" as="h5"><div className="titulo-text">{weather.name}, {weather.sys.country}</div><br />
+          <div className="data">{dias[data.getDay()]}, {str_data}</div></Card.Header>
+        <Card.Body className="body-center">
           <Card.Text>
-            <a>Temperatuta atual: <Badge bg="secondary" pill bg="warning" text="dark">{weather['main']['temp']}°</Badge></a><br />
-            <a>Temperatura Maxima: <Badge bg="secondary" pill bg="danger" text="dark">{weather['main']['temp_max']}°</Badge> </a><br />
-            <a>Temperatura Minima: <Badge bg="secondary" pill bg="info" text="dark">{weather['main']['temp_min']}°</Badge> </a><br />
+            <Badge className="temp" bg="secondary" bg="warning" text="dark">{Math.trunc(weather.main.temp)}°c</Badge><br />
             <div>
-              <a>Pressão: <Badge bg="secondary" pill bg="success" text="dark">{weather['main']['pressure']} hpa</Badge> </a><br />
-              <a>Humidade: <Badge bg="secondary" pill bg="secondary" text="dark">{weather['main']['humidity']}%</Badge></a>
-              <div>
-                <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Humidade: {weather['main']['humidity']}%</Tooltip>}>
-                  <ProgressBar striped variant="info" now={weather['main']['humidity']} />
-                </OverlayTrigger>
-              </div>
+              <Card.Img className="image-temp" variant="top" src={process.env.PUBLIC_URL + '/icons/' + weather.weather[0].icon + '.png'} />
+            </div>
+            <div className="descricao">{letraMaiuscula(weather.weather[0].description)}</div>
+            <div className="temp-max-min">{Math.trunc(weather.main.temp_min)}°c / {Math.trunc(weather.main.temp_max)}°c </div><br />
+            <div>
+              <Card.Title className="progress-text">Humidade</Card.Title>
+              <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{weather['main']['humidity']}%</Tooltip>}>
+                <ProgressBar striped variant="info" now={weather['main']['humidity']} />
+              </OverlayTrigger>
             </div>
           </Card.Text>
         </Card.Body>
